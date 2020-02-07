62257b07306b9a1ee9e879bf0c8ec380e0770c92
andrewkaldani <59716037+andrewkaldani@users.noreply.github.com>
2020-02-07 15:38:43 -0800

    Create van.cpp

---
+++ Lab 4/van.cpp
@@ -0,0 +1,29 @@
+#include "van.h"
+
+    Van::Van():Automobile(){
+        seats_ = 0;
+    }
+    Van::Van(std::string imake, std::string imodel, int iyear, double iprice, int istall, int iseats):Automobile(imake,imodel,iyear,iprice,istall){
+        seats_ = iseats;
+    }
+    bool Van::park(bool lot[], int size){
+        if (seats_ <= 7){
+            return Automobile::park(lot, size);
+        }
+        else {
+            int spot=size;
+            for(int i=0; i<size; i++){
+                if(!lot[i] && !lot[i-1])
+                    spot = i - 1;
+            }
+            if(spot>=size)
+            return false;
+            else{
+                lot[spot] = true;
+                lot[spot + 1] = true;
+                set_stall(spot);
+                return true;
+            }
+        }
+    }
+
